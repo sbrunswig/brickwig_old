3afc9a56b41b576604430c1a8e07f61277b71c28
Scott Brunswig <sbrunswig@gmail.com>
2014-12-12 20:58:36 -0600

    Delete hello.js

--- hello.js
+++
@@ -1,6 +0,0 @@
-function Hello($scope, $http) {
-    $http.get('http://rest-service.guides.spring.io/greeting').
-        success(function(data) {
-            $scope.greeting = data;
-        });
-}
