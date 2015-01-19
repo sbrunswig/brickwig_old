6f31570d855ecbb97e4751c7bbdfd930567ee95c
Scott Brunswig <sbrunswig@B5LIS001.local>
2015-01-19 07:13:09 -0600

    major update

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var notify = require('gulp-notify');
 
-gulp.task('default', function () {
-    gulp.src('./assets/css/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./assets/css'));
+gulp.task('styles', function() {
+  return gulp.src('./css/brickwig.scss')
+    .pipe(sass({ style: 'expanded' }))
+    .pipe(gulp.dest('./css'))
+    .pipe(notify({ message: 'Styles task complete' }));
 });
+
+gulp.task('default', function() {
+    gulp.start('styles');
+});
+
+gulp.task('watch', function() {
+  	// Watch .scss files
+  	gulp.watch('./css/*.scss', ['styles']);
+});
+
+
