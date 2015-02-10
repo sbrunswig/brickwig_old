a04339640ec7f7420ffa951815c6d551f0e16002
Scott Brunswig <sbrunswig@gmail.com>
2015-02-10 11:45:18 -0600

    style and js

---
+++ preview/js/SpringCM.UI.Preview.js
@@ -0,0 +1,86 @@
+﻿(function(global)
+{
+	///Register SpringCM namespace.
+	if (typeof (global.SpringCM) != "object")
+	{
+		//Create only if it does not exist
+		if (typeof (global.SpringCM) == "undefined")
+		{
+			global.SpringCM = {};
+		}
+	}
+
+
+	///Register SpringCM-Preview element.
+	///<springcm-preview></springcm-preview>
+	var proto = Object.create(HTMLElement.prototype);
+	proto.createdCallback = function()
+	{
+		//Just for testing; real version will get the pages from a document page count.
+		//var pages = this.getAttribute("pages") || 1;
+
+		this.innerHTML = "";
+		//for (var i = 0; i < pages; i++)
+		//{
+		//	this.innerHTML += '<div class="pages"></div>';
+		//}
+
+	};
+
+	global.SpringCM.DocumentPreview = global.document.registerElement("springcm-preview", { prototype: proto });
+
+
+	// preview component js object
+	var s, PreviewComponent = {
+		settings: {
+			threshold: 500,
+			page: "div.page",
+			pagePlaceholder: "page--placeholder",
+			shadow: "shadow--z-1",
+			errorImage: "images/404.png"
+		},
+
+		init: function () {
+			s = this.settings;
+			s.pageItem = $("img.page__item");
+
+			this.bindUIActions();
+		},
+
+		bindUIActions: function () {
+			s.pageItem.unveil(s.threshold, function () {
+				$(this).on("load", function () {
+					var ratioHeight = $(this)[0].height; //get height of new image
+					var ratioWidth = $(this).parents(s.page).outerWidth(); //get width of image container
+					var ratio = (ratioHeight / ratioWidth) * 100 + '%'; //get new ratio
+					//$(this).addClass(s.shadow); //add style to image
+					$(this).parents(s.page).css({ 'padding-top': ratio }).removeClass(s.pagePlaceholder); //resize container and remove style
+				}).on("error", function () {
+					$(this).unbind("error").attr("src", s.errorImage);
+				});
+			});
+		},
+	}
+
+	// lazy load images into springcm-preview component
+	var url = 'http://brickwig.com/preview/image.json?callback=?';
+	$.ajax({
+		type: 'GET',
+		url: url,
+		async: false,
+		jsonpCallback: 'jsonCallback',
+		contentType: "application/json",
+		dataType: 'jsonp',
+		success: function (data) {
+			$.each(data.images, function (i, image) {
+				$("<img>").addClass("page__item").attr("src", "images/spinner.svg").attr("data-src-small", data.images[i].imageurlS).attr("data-src-medium", data.images[i].imageurl).attr("data-src-high", data.images[i].imageurl).appendTo($("<div>").addClass("page page--placeholder").appendTo("springcm-preview"));
+			});
+			PreviewComponent.init();
+		},
+		error: function (e) {
+			//TODO: add error message
+		}
+	});
+
+
+})(window);
---
+++ preview/js/jquery.unveil.js
@@ -0,0 +1,67 @@
+﻿/**
+ * jQuery Unveil
+ * A very lightweight jQuery plugin to lazy load images
+ * http://luis-almeida.github.com/unveil
+ *
+ * Licensed under the MIT license.
+ * Copyright 2013 Luís Almeida
+ * https://github.com/luis-almeida
+ */
+
+;(function($) {
+	$.fn.unveil = function(threshold, callback) {
+		var $w = $(window),
+			th = threshold || 0,
+			retina = window.devicePixelRatio > 1,
+			imageVersion = getImageVersion(),
+			attrib = retina? "data-src-retina" : "data-src-" + imageVersion,
+			images = this,
+			loaded;
+
+		this.one("unveil", function() {
+			var source = this.getAttribute(attrib);
+			source = source || this.getAttribute("data-src-" + imageVersion);
+			if (source) {
+				this.setAttribute("src", source);
+				if (typeof callback === "function") callback.call(this);
+			}
+		});
+
+		function unveil() {
+			var inview = images.filter(function() {
+				var $e = $(this);
+				//if ($e.is(":hidden")) return;
+
+				var wt = $w.scrollTop(),
+				wb = wt + $w.height(),
+				et = $e.offset().top,
+				eb = et + $e.height();
+
+				return eb >= wt - th && et <= wb + th;
+			});
+
+			loaded = inview.trigger("unveil");
+			images = images.not(loaded);
+		}
+
+		/* added by springcm */
+		function getImageVersion() {
+			var width = window.innerWidth;
+
+			// sizes: small = 320, medium = 640, high = 720
+			if (width > 320 && width <= 640) {
+				return "medium";
+			} else if (width > 640) {
+				return "high";
+			} else {
+				return "small"; // default version
+			}
+		}
+
+		$w.on("scroll.unveil resize.unveil lookup.unveil", unveil);
+
+		unveil();
+
+		return this;
+	};
+})(window.jQuery);
